88210d13356f7de2d4247dc66b2b6d2250df6809
nicktrimborn <nick.trimborn@gmail.com>
2017-02-11 00:03:02 +0930

    Add project files.

---
+++ Carlift.ino
@@ -0,0 +1,38 @@
+ 
+
+
+void setup()
+{
+
+  /* add setup code here */
+
+}
+
+void loop()
+{
+
+  /* add main program code here */
+
+}
+
+class liftPost
+{
+public:
+	MyClass();
+	~MyClass();
+
+private:
+	int encoderCount;
+	int state; //0 = stopped, 1 = Move Up, 2 = Move Down
+
+
+
+};
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
